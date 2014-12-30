a4522e2399daf4f45945d71ef251c25ec3eb0b58
iva-toshkova <iva.lubenova@gmail.com>
2014-12-30 22:28:39 +0100

    Added some Javadoc

--- Software Dev/src/item/NumberPoints.java
+++ Software Dev/src/item/NumberPoints.java
@@ -5,30 +5,76 @@ public class NumberPoints {
 	private long id;
 	private String name;
 	
+	/**
+    * Used to return the name in string format
+    *
+    * @return name
+    *         returns the name
+    * 
+    */
 	public String toString() {
 		return name;
 	}
-
+    
+    /**
+    * Gets number points
+    *
+    * @return numberPoints
+    *         returns the number points
+    * 
+    */
 	public int getNumberPoints() {
 		return numberPoints;
 	}
-
+    
+    /**
+    * Sets number points
+    *
+    * @param numberPoints
+    * 
+    */
 	public void setNumberPoints(int numberPoints) {
 		this.numberPoints = numberPoints;
 	}
-
+    
+    /**
+    * Gets name
+    *
+    * @return name
+    *         returns the name
+    * 
+    */
 	public String getName() {
 		return name;
 	}
-
+    
+    /**
+    * Sets name
+    *
+    * @param name
+    * 
+    */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+    /**
+    * Gets id
+    *
+    * @return id
+    *         returns the id
+    * 
+    */
 	public long getId() {
 		return id;
 	}
-
+    
+    /**
+    * Sets id
+    *
+    * @param id
+    * 
+    */
 	public void setId(long id) {
 		this.id = id;
 	}
